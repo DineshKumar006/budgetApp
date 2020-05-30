972789bf968523a28d5166fe6283f84621c74f07
Dinesh_OYO <oyo@ADMINs-MacBook-Pro.local>
2020-05-30 14:26:19 +0530

    init

--- server/server.js
+++ server/server.js
@@ -1,22 +1,22 @@
-const express=require('express');
-const cors=require('cors')
-const PORT=8080;
-const path=require('path');
+// const express=require('express');
+// const cors=require('cors')
+// const PORT=8080;
+// const path=require('path');
 
-const app=express();
+// const app=express();
 
-const publicPath=path.join(__dirname,'../public')
+// const publicPath=path.join(__dirname,'../public')
 
 
-app.use(express.static(publicPath))
-app.use(cors())
+// app.use(express.static(publicPath))
+// app.use(cors())
 
-app.use(express.json())
+// app.use(express.json())
 
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(publicPath,'index.html'))
-})
+// app.get('*',(req,res)=>{
+//     res.sendFile(path.join(publicPath,'index.html'))
+// })
 
-app.listen(PORT,()=>{
-    console.log('server is running on port',PORT)
-})
+// app.listen(PORT,()=>{
+//     console.log('server is running on port',PORT)
+// })
